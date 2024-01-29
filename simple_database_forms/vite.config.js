2362062873660dfbd63671b9940fd7e78099d574
Alireza Khorramshekooh <alireza.khorramshekooh@gmail.com>
2024-01-29 19:52:07 +0330

    desktop form added

--- simple_database_forms/vite.config.js
+++ simple_database_forms/vite.config.js
@@ -12,6 +12,7 @@ import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [
     vue({
       template: { transformAssetUrls }
